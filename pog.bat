4161eae7449a2bc940d0344cccb7c0bdcf1da916
4ce <88108711+4cecoder@users.noreply.github.com>
2022-10-10 14:08:40 -0400

    Update pog.bat

--- pog.bat
+++ pog.bat
@@ -5,6 +5,8 @@ md %1
 endlocal
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+refreshenv
+
 choco feature enable -n=allowGlobalConfirmation
 
 ECHO "Pogging up WINDOWS! Please wait..."
@@ -13,10 +15,19 @@ choco install git go rust python3 ffmpeg mpv make
 mkdir pog-projects
 cd pog-projects
 ECHO "Fetching the poggiest projects..."
+
+rm -rf "/usr/local/share/ani-cli" "/usr/local/bin/ani-cli" "/usr/local/bin/UI" /usr/local/bin/player_* #If some of these aren't found, it's not a problem
+git clone "https://github.com/pystardust/ani-cli.git" && cd ./ani-cli
+cp ./ani-cli /usr/bin
+cd .. && rm -rf ./ani-cli
+
 git clone https://github.com/mov-cli/mov-cli
+
 git clone https://github.com/manga-g/manga-g
+cd ./manga-g/cmd/manga-g/
+go build && cp ./manga-g /usr/bin
+cd ../../../ && rm -rf manga-g
+
 git clone https://github.com/ayntgl/discordo
-
-
-
- 
+cd ./discordo
+make build && cp ./discordo /usr/bin && cd .. && rm -rf ./discordo
