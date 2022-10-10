dce4ca9387c7fdba7b85bf393d740e44bc735734
4ce <88108711+4cecoder@users.noreply.github.com>
2022-10-10 13:51:42 -0400

    Update pog.bat

--- pog.bat
+++ pog.bat
@@ -1,11 +1,22 @@
 ECHO "Are we Pog Yet?"
+@echo off
+setlocal enableextensions
+md %1
+endlocal
+
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 ECHO "Pogging up WINDOWS! Please wait..."
 choco install git go rust python3 ffmpeg mpv make
 
 
+mkdir pog-projects
+cd pog-projects
+ECHO "Fetching the poggiest projects..."
 git clone https://github.com/mov-cli/mov-cli
 git clone https://github.com/manga-g/manga-g
 git clone https://github.com/ayntgl/discordo
 
+
+
+ 
