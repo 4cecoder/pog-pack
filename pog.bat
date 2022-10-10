4cbc949e8a309d56fda7295e07ca2642ce331da1
4ce <88108711+4cecoder@users.noreply.github.com>
2022-10-10 13:56:09 -0400

    Update pog.bat

--- pog.bat
+++ pog.bat
@@ -5,10 +5,10 @@ md %1
 endlocal
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco feature enable -n=allowGlobalConfirmation
 
 ECHO "Pogging up WINDOWS! Please wait..."
 choco install git go rust python3 ffmpeg mpv make
-
 
 mkdir pog-projects
 cd pog-projects
