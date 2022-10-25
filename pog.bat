66a764f8f00e97e8e2ec679cee2c5e7e34dd1956
4ce <88108711+4cecoder@users.noreply.github.com>
2022-10-24 22:02:42 -0400

    Update pog.bat

--- pog.bat
+++ pog.bat
@@ -3,7 +3,6 @@ ECHO "Are we Pog Yet?"
 setlocal enableextensions
 md %1
 endlocal
-\\\ECHO "CHOCOLATE..."
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 refreshenv
 choco feature enable -n=allowGlobalConfirmation
